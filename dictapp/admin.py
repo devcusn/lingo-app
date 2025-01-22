18377d39e2f3663ced425cd83dc3c2e26e79d3aa
Halil Tunc <hitunc@outlook.com>
2025-01-22 18:50:28 +0300

    feat: models add to admin panel

--- dictapp/admin.py
+++ dictapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Word, WordUser
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('word', 'created_at')
+    search_fields = ('word',)
+    list_filter = ('created_at',)
+
+
+@admin.register(WordUser)
+class WordUserAdmin(admin.ModelAdmin):
+    list_display = ('word', 'user', 'created_at')
+    list_filter = ('created_at', 'user')
+    search_fields = ('word__word', 'user__username')
